f4f3eed3f23c17ebb82213ad450adf286d2e8982
Wesses <wesses1999@gmail.com>
2022-05-26 03:24:58 +0300

    connect to custom API

--- src/Components/Music/Music.js
+++ src/Components/Music/Music.js
@@ -1,5 +1,5 @@
 import classes from "./Music.module.css";
-import bolt from '../../audio/plenka - Visitor.mp3';
+import bolt from '../../assets/audio/plenka - Visitor.mp3';
 
 const Music = () => {
     return (
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,15 +1,12 @@
 import styles from "./Users.module.css"
-
+import * as axios from "axios";
+import userPhoto from "../../assets/images/userPhoto.png"
 
 const Users = (props) => {
-
-    if (props.users.length === 0) {
-        props.setUsers([
-            {id: 0, followed: true, photoUrl: "https://st2.depositphotos.com/48751036/47697/v/600/depositphotos_476979432-stock-illustration-happy-man-laughs-human-emotions.jpg",fullName: "Dmitriy K.", status: "I am looking for a Job right now...", location: {city:"New-York", country:"United States"}},
-            {id: 1, followed: true, photoUrl: "https://www.reallusion.com/crazytalk/includes/images/Cartoon%20Animator%20(720x405).jpg", fullName: "Svetlana D.", status: "I am so pretty", location: {city:"London", country:"Great Britain"}},
-            {id: 2, followed: false, photoUrl: "https://cdn3d.iconscout.com/3d/premium/thumb/man-with-bun-3311666-2754835.png", fullName: "Sergei S.", status: "I like football...", location: {city:"Kiev", country:"Ukraine"}},
-            {id: 3, followed: false, photoUrl: "https://ru-static.z-dn.net/files/dfd/0b0fc9c948fc1d3aa35c42b88e7dadc5.jpg", fullName: "Andrew T.", status: "I am free to help you..", location: {city:"Odessa", country:"Ukraine"}},
-        ])
+    if (props.users.length === 0){
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
     }
 
     return <div>
@@ -17,7 +14,7 @@ const Users = (props) => {
             <div key ={u.id}>
                 <span>
                     <div>
-                        <img alt={""} src={u.photoUrl} className={styles.userPhoto}/>
+                        <img alt={""} src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed ?
@@ -27,12 +24,12 @@ const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>
